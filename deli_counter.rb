71b02c5f61bf80136213832f8cdc97b6be06dff3
Alex Engelhart <asengelhart@yahoo.com>
2019-12-10 19:38:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line(deli)
+  result = "The line is currently"
+  if deli.size == 0 
+    result += " empty."
+  else 
+    result += ":"
+    deli.each_with_index do |name, index|
+      place = index + 1 
+      result += " #{place}. #{name}"
+    end 
+  end 
+  puts result
+end 
+
+def take_a_number(name)
+end 
