f30ab9c3fbbf03e102ddaa105d8e01c177b7bf33
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-12 21:45:39 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  line_array = []
+  if array.length == 0 
+    puts "The line is currently empty."
+  elsif 
+    array.each_with_index(1) do |value, index|
+    line_array.push("#{index}. #{value}")
+  end
+  puts "The line right now is: #{line_array.join(" ")}"
+ end 
+end 
+    
+   
+
+
