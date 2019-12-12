05623a10cd0befa8b94b60e9727c07c16d66b837
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-12 22:22:08 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
+
+def line(katz_deli)
+  
+  if katz_deli.length == 0   
+    puts "The line is currently empty."
+    
+  else  
+       ppl_on_line = [ "The line is currently:"] 
+       katz_deli.each_with_index do |customer, index|
+       ppl_on_line << [" #{index + 1}. #{customer}"]
+      
+    end
+    puts ppl_on_line.join
+  end
+end
+
+
+def take_a_number(katz_deli, custy)
+  
+end
+
+
 # Write your code here.
