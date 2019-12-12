3b2b0e88fba34c21ba741d6484d50265ad0f0d10
github username <github email address>
2019-12-12 14:34:10 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,31 @@
-
-
-    # katz_deli.map.with_index do |customer, position|
-    #     puts "The line is currently: #{position+=1}. #{customer}"
-    # end
-def person(katz_deli)
-    katz_deli.each_with_index do |customer, position|
-        puts "#{position+=1}. #{customer}"
-    end
-end
+require 'pry'
 
 def line(katz_deli)
     if katz_deli.length.zero? 
         puts "The line is currently empty."
     elsif katz_deli.length > 0
-        puts "The line is currently:" << person(katz_deli)
-        
-        
+        sentence = "The line is currently:" 
+        katz_deli.each_with_index do |customer, position|
+            sentence << " #{position+=1}. #{customer}"
+        end
+        puts sentence
     end
 end
 
-def take_a_number
-
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-
+def now_serving(line)
+    if line.length.zero?
+        #if nobody is in line, should call out "There is nobody waiting to be served!"
+        puts "There is nobody waiting to be served!"
+    elsif 
+        puts "Currently serving #{line.first}."
+        #puts to next person in line
+        
+    end
+    # remove them from front
+    line.shift
 end
