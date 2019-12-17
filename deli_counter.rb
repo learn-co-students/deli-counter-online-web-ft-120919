8522f62f6557abcace8a3ba6639a3a0af9ee91ff
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-17 03:28:12 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
+
 # Write your code here.
